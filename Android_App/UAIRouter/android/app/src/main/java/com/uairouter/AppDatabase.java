6b1a4a5e2d8a38fe55f10ce97c851b5bd6893c15
Pikkati <pikkati@protonmail.com>
2026-02-19 01:12:07 +0000

    chore(submodule): record local deletions from workspace

--- Android_App/UAIRouter/android/app/src/main/java/com/uairouter/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.uairouter;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {AnalyticsData.class}, version = 1, exportSchema = false)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract AnalyticsDao analyticsDao();
-}
